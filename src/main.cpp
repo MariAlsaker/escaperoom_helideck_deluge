60acb43bdc5b7e4ee9b70cb3236bed1cc918b247
Mari M. Alsaker <mmalsaker@gmail.com>
2023-08-08 13:38:18 +0200

    Init repo, first working

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+
+#define REED1 9
+#define REED2 8
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+uint32_t timer = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(REED1, INPUT);
+  pinMode(REED2, INPUT);
+  //Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("Manual mode");
+  timer = millis();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(millis()-timer > 100){ // Only every 10 ms
+    timer = millis();
+    if(digitalRead(REED1)&&digitalRead(REED2)){
+      lcd.clear();
+      lcd.print("Deluge started");
+      lcd.setCursor(0,1);
+      lcd.print("code: 392");
+    }
+    else{
+      //lcd.print("Manual mode");
+    }
+  }
+  
+}
